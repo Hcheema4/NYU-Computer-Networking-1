2ffefcc2912dfc7be2d33f7ab521cee1293c92c2
Collin Stilwell <cns9383@nyu.edu>
2021-09-07 17:01:33 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
 
 
     if question ==  "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "The student should type the answer here"
+        answer = "mtls"
 
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
@@ -26,13 +26,13 @@ def welcome_assignment_answers(question):
         answer = "42b76fe51778764973077a5a94056724"
 
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "Yes"
+        answer = "No"
 
     elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 2
+        answer = 5
 
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 3
+        answer = 4
 
     return(answer)
 
